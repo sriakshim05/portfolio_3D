e88c4e9878fec6dc37e807f868afbdad91190f97
Abiram08 <akashsriakash8@gmail.com>
2025-12-21 23:49:31 +0530

    src and index file are pusing

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,22 @@
+ import React from 'react'
+ 
+ const Navbar = () => {
+  const [isOpen,setIsOpen]=useState(false);
+   return (
+     <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
+      <div className='max-w-7xl mx-auto'>
+        <div className='flex justify-between items-center py-5 mx-auto c-space'>
+        <a href ='/' className='text-neutral-400 font-bold text-xl hover:text-white transition-colors '>
+        Akash 
+        </a>
+        <button onClick={toggleMenu}>
+          <img src={isOpen ? "/assets/menu.svg" : "/assets/close.svg"} alt ='toggle' className='w-6 h-6' />
+
+        </button>
+      </div>
+      </div>
+     </header>
+   ) 
+ }
+ 
+ export default Navbar
