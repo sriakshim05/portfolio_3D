0fdd0ef9e3c39816a35e5eae2cfa858d68a63fab
akash <akashsriakash8@gmail.com>
2025-12-22 22:43:45 +0530

    navbar updated

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,7 +1,10 @@
  import React from 'react'
- 
+ import { useState } from "react";
+
  const Navbar = () => {
   const [isOpen,setIsOpen]=useState(false);
+  const toggleMenu=()=>setIsOpen((prevIsOpen)=>!prevIsOpen);
+  
    return (
      <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
       <div className='max-w-7xl mx-auto'>
@@ -9,10 +12,13 @@
         <a href ='/' className='text-neutral-400 font-bold text-xl hover:text-white transition-colors '>
         Akash 
         </a>
-        <button onClick={toggleMenu}>
-          <img src={isOpen ? "/assets/menu.svg" : "/assets/close.svg"} alt ='toggle' className='w-6 h-6' />
+        <button onClick={toggleMenu} className="text-neutral-400 hover:text-white foucs:outline-none sm:hidden flex" aria-label="Toggle menu">
+          <img src={isOpen ? "/assets/close.svg" : "/assets/menu.svg"} alt ='toggle' className='w-6 h-6' />
 
         </button>
+        <nav className='sm:flex hideen'>
+
+        </nav>
       </div>
       </div>
      </header>
