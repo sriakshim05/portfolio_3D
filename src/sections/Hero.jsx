3ba1d32858121f3475b1e60e8bb04abaf406e8a5
sriakshim05 <sriakashim05@gmail.com>
2025-12-25 16:04:32 +0530

    some changes in Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { PerspectiveCamera } from '@react-three/drei'
+import HackerRoom from '../components/HackerRoom.jsx'
+import CanvasLoader from '../components/CanvasLoader.jsx'
+import { DirectionalLight } from 'three'
 
 export const Hero = () => {
   return (
-   <section className='min-h-screen w-full flex flex-col relative'>
-    <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
-        <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>Hi Iam Akash 
-            <spam className='waving-hand'>👋</spam> 
-                </p>
-                <p className='hero_tag text-gray_gradient text-center '>Bulding Apps & Web Site</p>
-    </div>
-   </section>
-   
+    <section className="min-h-screen w-full flex flex-col relative">
+
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+          Hi I am Akash 
+          <span className="waving-hand"> 👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient text-center">
+          Building Apps & Websites
+        </p>
+      </div>
+
+      <div className="w-full h-full absolute inset-0">
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader/>}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HackerRoom  scale ={0.05} postion={[0,0,0]} roation={[0,280,0]} />
+            <ambientLight intensity={1}/>
+            <directionalLight position={[10,10,10]} intensity={0.5}/>
+          </Suspense>
+        </Canvas>
+      </div>
+
+    </section>
   )
 }
